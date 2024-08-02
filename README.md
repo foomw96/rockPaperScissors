85cefdd65ff4d487c3ef491ce15c2b5a0e8358bd
foomw96 <111436424+foomw96@users.noreply.github.com>
2024-08-02 17:37:27 +0800

    Write function to generate computer choice

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rockPaperScissors
 Odin project - Rock Paper Scissors
+
+## About
+Rock paper scissors game against the computer
+Played in the console
--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+console.log(getComputerChoice());
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+
+    if (choice === 0) {
+        return 'rock';
+    } else if (choice === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
